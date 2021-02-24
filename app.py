e449dd7c713fece00d9c2ba2bb4c8c24088eb792
ConorMcElvogue <conormcelvogue@gmail.com>
2021-02-24 23:00:55 +0000

    adding back name

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Homepage
 @app.route('/')
 def hello():
-    return "<h3>Hello, Udacity! Welcome to my Conor's Capstone project.<h3>"
+	return "<h3>Hello, Udacity! Welcome to my Capstone project.<h3>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # Run on port 80
