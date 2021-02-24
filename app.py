846801a2a6a07c1637cd06eec90097b64e760883
ConorMcElvogue <conormcelvogue@gmail.com>
2021-02-24 23:44:27 +0000

    update for app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Homepage
 @app.route('/')
 def hello():
-    return "<h3>Hello, Udacity! Welcome to my Capstone project.<h3>"
+    return "<h3>Conor's hello Wold - Udacity Devops.<h3>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # Run on port 80
